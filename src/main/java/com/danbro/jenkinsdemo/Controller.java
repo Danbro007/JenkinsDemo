2cb45f53e2a49bf723b83bdde0f6f55b3bc11ee7
danbro <710170342@qq.com>
2020-03-31 13:37:57 +0800

    second commit

---
+++ src/main/java/com/danbro/jenkinsdemo/Controller.java
@@ -0,0 +1,19 @@
+package com.danbro.jenkinsdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Classname Controller
+ * @Description TODO
+ * @Date 2020/3/31 13:36
+ * @Author Danrbo
+ */
+@RestController
+public class Controller {
+    @GetMapping("")
+    public String hello(){
+        return "hello";
+    }
+
+}
